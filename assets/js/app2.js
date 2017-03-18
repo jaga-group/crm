52e911b5c3467d122c2e23d0aaf080d45f9ddfa0
Ariel <ariel.m.little@gmail.com>
2017-03-18 10:18:19 -0400

    using firebase ui

---
+++ assets/js/app2.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyB24kr0uOAMqt674qN_1z7fIe8TR_1Ac84",
+        authDomain: "jagaproject-2740a.firebaseapp.com",
+        databaseURL: "https://jagaproject-2740a.firebaseio.com",
+        storageBucket: "jagaproject-2740a.appspot.com",
+        messagingSenderId: "801684809525"
+    };
+    firebase.initializeApp(config);
+
+
+    var uiConfig = {
+        signInSuccessUrl: 'reservationinfo.html',
+        signInOptions: [
+            // Leave the lines as is for the providers you want to offer your users.
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+            firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+            firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+            firebase.auth.GithubAuthProvider.PROVIDER_ID,
+            firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        // Terms of service url.
+        tosUrl: '<your-tos-url>'
+    };
+
+    // Initialize the FirebaseUI Widget using Firebase.
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // The start method will wait until the DOM is loaded.
+    ui.start('#firebaseui-auth-container', uiConfig);
+
+    initApp = function() {
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                // User is signed in.
+                var displayName = user.displayName;
+                var email = user.email;
+                var emailVerified = user.emailVerified;
+                var photoURL = user.photoURL;
+                var uid = user.uid;
+                var providerData = user.providerData;
+                user.getToken().then(function(accessToken) {
+                    document.getElementById('sign-in-status').textContent = 'Signed in';
+                    document.getElementById('sign-in').textContent = 'Sign out';
+                    document.getElementById('account-details').textContent = JSON.stringify({
+                        displayName: displayName,
+                        email: email,
+                        emailVerified: emailVerified,
+                        photoURL: photoURL,
+                        uid: uid,
+                        accessToken: accessToken,
+                        providerData: providerData
+                    }, null, '  ');
+                });
+            } else {
+                // User is signed out.
+                document.getElementById('sign-in-status').textContent = 'Signed out';
+                document.getElementById('sign-in').textContent = 'Sign in';
+                document.getElementById('account-details').textContent = 'null';
+            }
+        }, function(error) {
+            console.log(error);
+        });
+    };
+
+    window.addEventListener('load', function() {
+        initApp()
+    });
+
+}); // End of Document Ready //
